a8a43afba25cc2f5662273b6031bda127f58f98f
Gregorius <gregvidy@gmail.com>
2020-04-16 09:52:06 +0700

    modify a little

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-#export TRAINING_DATA=input/train_preprocessed.csv
+export TRAINING_DATA=input/train_preprocessed.csv
 export TEST_DATA=input/test_preprocessed.csv
 export MODEL=$1
 
-#FOLD=0 python -m script.training
-#FOLD=1 python -m script.training
-#FOLD=2 python -m script.training
-#FOLD=3 python -m script.training
-#FOLD=4 python -m script.training
+FOLD=0 python -m script.training
+FOLD=1 python -m script.training
+FOLD=2 python -m script.training
+FOLD=3 python -m script.training
+FOLD=4 python -m script.training
 
 python -m script.predict
